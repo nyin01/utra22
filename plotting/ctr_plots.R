202c2cb5e1c899f7f943c6a6aacd3a1a6ff48077
Neal Yin <79818377+nyin01@users.noreply.github.com>
2022-09-05 15:39:35 -0400

    Delete plotting directory

--- plotting/ctr_plots.R
+++
@@ -1,9 +0,0 @@
-require(tidyverse)
-dt <- read.csv("results/all_ctr.csv")
-
-dt %>% select(Control, K, NPfix)
-
-scat <- ggplot(dt, aes(x = K, y = NPfix, color = Control, shape = Control)) +
-  geom_point(size = 2) +
-  scale_x_log10() 
-scat
